14c95138efad9e60d470186e46b39774f02df8d6
prabhat <akoup66@gmail.com>
2021-10-14 11:08:40 +0545

    commited

--- test/test_add_flower.py
+++ test/test_add_flower.py
@@ -33,3 +33,15 @@ def test_add_new_in_stock(add):
     assert len(STOCK) == length + 1
     assert STOCK[-1] == {'flower_name': "Sunflower", 'quantity': 10, "price": 4.5}
     STOCK.pop()
+
+
+def test_add_new_in_stock_negative(add):
+    """ asserts if new items is added or not and also the correct value is added
+    :param: add: it's a tuple of two objects of AddFlower, where add[0] = AddFLower("Sunflower")
+    """
+    length = len(STOCK)
+    # here first parameter is for quantity and second for price while flower name is initialised already
+    for i in [(0, 1.1), ("we", "EW"), (0, 0)]:
+        add[1].add_new_in_stock(10, 4.5)
+    assert not STOCK[-1] == {'flower_name': "Sunflower", 'quantity': 10, "price": 4.5}
+    STOCK.pop()
